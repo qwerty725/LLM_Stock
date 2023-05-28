0c6233aeaa8d47eb068c46598dd9b9d460a4794d
samyty1530 <samantha.t.yang@gmail.com>
2023-05-28 18:02:32 -0400

    full of spiders!

--- suhmanfuh.py
+++ suhmanfuh.py
@@ -15,6 +15,37 @@ class Stock():
         self.name = name
         self.price = price
 
+    def scrape(self):
+        # Make a request to the Google Finance website
+        # url='https://www.bbc.com/news'
+        # response = requests.get(url)
+
+        # soup = BeautifulSoup(response.text, 'html.parser')
+        # headlines = soup.find('body').find_all('h3')
+        # for x in headlines:
+        #     print(x.text.strip())
+
+
+        url = "https://finance.google.com/"
+        response = requests.get(url)
+
+        # Parse the HTML response
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Find all the headline elements that contain the word "hours ago"
+       # headlines = soup.find('body')
+        headlines = soup.find_all("div")
+            
+        # find_all("Yfwt5")
+        # soup.find_all("Yfwt5")
+                                 #  class_="story-title", text=lambda text: text.find("hours ago") != -1)
+       # soup.find('body').find_all(
+
+        # Print the headlines
+        for headline in headlines:
+            if "Yfwt5" in headline.class_:
+                  print(headline.text.strip())
+    
 if __name__ == '__main__':
     test = Stock("sam", 25)
     print(test.price)
@@ -24,3 +55,4 @@ if __name__ == '__main__':
     for line in reader:
         stock_list.append(line[0])
     print(stock_list[:10])
+    test.scrape()
