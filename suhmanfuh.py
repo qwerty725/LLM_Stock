b027a73195db93d922b85de992105b8b56ba3e8e
Eric Zhang <ericzhang725@gmail.com>
2023-05-28 16:05:42 -0400

    initial commit, garbage

---
+++ suhmanfuh.py
@@ -0,0 +1,17 @@
+def median(pool):
+    copy = sorted(pool)
+    size = len(copy)
+    if size % 2 == 1:
+        return copy[int((size - 1) / 2)]
+    else:
+        return (copy[int(size/2 - 1)] + copy[int(size/2)]) / 2
+class Stock():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+if __name__ == '__main__':
+    test = Stock("sam", 25)
+    print(test.price)
+    median(25)
+    Stock()
